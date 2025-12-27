a3f1e590481cb2b5fe6485bb509f8229892d6da1
khashayar <khashayarmobarezbusiness@gmail.com>
2025-12-27 16:47:24 +0330

    until const and let in ts

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello typescript  1')
+let myVar = 'khashayar mobarrez'
+
+console.log('testing', myVar)
+
+myVar = 'this if my first var'
+
+console.log('testing2', myVar)
+
+myVar = 30
+
+console.log('testing3', myVar)
