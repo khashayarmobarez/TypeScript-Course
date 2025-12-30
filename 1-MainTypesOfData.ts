1caacb868fa06cbcf541e2066076523e2ee7f230
khashayar <khashayarmobarezbusiness@gmail.com>
2025-12-30 18:11:05 +0330

    typescript main types

---
+++ 1-MainTypesOfData.ts
@@ -0,0 +1,22 @@
+// main types of data:
+// 1- string
+// 2- number (integer)
+// 3- boolean
+
+// string example
+let BlogTitle : string = 'string example';
+console.log(typeof BlogTitle)
+
+// number example
+let BlogLikesCount : number = 5
+console.log(typeof BlogLikesCount)
+
+// boolean example
+let isPublished: boolean = true;
+console.log(typeof isPublished)
+
+// you can set the type of a variable with type of another variable
+let user : typeof BlogTitle
+user = 'khashy'
+console.log("user type:",typeof user)
+
